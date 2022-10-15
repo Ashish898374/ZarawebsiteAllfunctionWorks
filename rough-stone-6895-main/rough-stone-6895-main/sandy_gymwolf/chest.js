d6485bc2054554f88889b6c3395bf60e418827ee
Ashish898374 <yadavashish347347@gmail.com>
2022-10-15 19:36:56 +0530

    second

---
+++ rough-stone-6895-main/rough-stone-6895-main/sandy_gymwolf/chest.js
@@ -0,0 +1,18 @@
+
+
+
+import footer from "../Homepage/footer.js"
+
+document.getElementById("footer-top").innerHTML=footer()
+
+
+import navbar from "../sandy_gymwolf/sandy_navbar.js"
+document.getElementById("nav").innerHTML=navbar()
+
+
+let data=JSON.parse(localStorage.getItem("userData"))
+  
+data.forEach(function(el){
+  console.log(el.name)
+document.getElementById("baljor").innerHTML=el.name
+})
